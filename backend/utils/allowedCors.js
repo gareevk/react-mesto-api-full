ddf37a152658272c9d49022a637c0f1f370943f1
Konstantin Gareev <gareevk@hotmail.com>
2022-05-29 17:41:15 +0300

    cors validation update

--- backend/utils/allowedCors.js
+++ backend/utils/allowedCors.js
@@ -2,6 +2,7 @@ const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
   'localhost:3000',
+  'https://avocado.nomoredomains.xyz',
 ];
 
 module.exports = allowedCors;
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://auth.nomoreparties.co';
+const BASE_URL = 'https://api.avocado.nomoreparties.sbs';
 
 export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`,
